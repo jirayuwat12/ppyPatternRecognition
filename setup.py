c1f25c9c0e33198ec73b173663cb54ccf744cad0
jirayuwat12 <jirayuwat12@gmail.com>
2024-01-15 18:39:34 +0700

    release first version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ppyPatternRecognition",
-    version="0.0.1",
+    version="0.0.1.1",
     author="Jirayuwat Boonchan",
     author_email="jirayuwat.dev@gmail.com",
     description="Pattern Recognition Package",
