7933cb926fd21367758f9935a44c5952e120e52c
jirayuwat12 <jirayuwat12@gmail.com>
2024-01-15 18:50:45 +0700

    .

--- setup.py
+++ setup.py
@@ -3,14 +3,29 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def get_requirements(fname : str):
+    '''
+    extract library name from `fname`
+    '''
+    with open(fname) as fp:
+        reqs = list()
+        for lib in fp.read().split("\n"):
+            # Ignore pypi flags and comments
+            if not lib.startswith("-") or lib.startswith("#"):
+                reqs.append(lib.strip())
+        return reqs
+
+
+install_requires = get_requirements("requirements.txt")
+
 setuptools.setup(
     name="ppyPatternRecognition",
-    version="0.0.0.2",
+    version="0.0.0.3",
     author="Jirayuwat Boonchan",
     author_email="jirayuwat.dev@gmail.com",
     description="Pattern Recognition Package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=[line.rstrip() for line in open('requirements.txt')],
+    install_requires=install_requires
 )
